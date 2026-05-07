99654f21a688418d8cd0460ec47bba366bfd96f4
Mohammed Airy Anwar <161919123+MohammedAiryAnwar@users.noreply.github.com>
2026-05-07 14:03:19 +0530

    Update API base URL to production endpoint

--- eMate_v2/frontend/src/services/api.js
+++ eMate_v2/frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
  // baseURL: 'http://localhost:5000/api',
-    baseURL: '',
+    baseURL: 'https://emate-backend-naj2.onrender.com',
 
 });
 
